0f432043da23c537b48c3ed8f2f7c7c1ba4611f8
Guilherme Conti Teixeira <68434142+Guilherme-jpg-oss@users.noreply.github.com>
2024-06-05 11:35:20 -0300

    Delete TrabalhoWEB2 (professor) directory

--- TrabalhoWEB2 (professor)/Trabalho Programação Web II/includes/nav.php
+++
@@ -1,8 +0,0 @@
-<div class="nav">
-    <a href="index.php">Home</a>
-    <a href="dashboard.php">Dashboard</a>
-    <a href="add_product.php">Cadastrar Produto</a>
-    <a href="view_products.php">Visualizar Produtos</a>
-    <a href="sales_report.php">Relatório de Vendas</a>
-    <a href="logout.php">Logout</a>
-</div>
